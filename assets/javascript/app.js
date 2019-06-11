5d6cc3d368050b37dfd83d7d84eb44e58c231e24
ahydorn <adamhydorn@gmail.com>
2019-06-11 13:37:42 -0700

    beginning of landing page html w/ materialize

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,48 @@
-// MusixMatch
-// Endpoint: [https://musixmatchcom-musixmatch.p.rapidapi.com/wsr/1.1/](https://musixmatchcom-musixmatch.p.rapidapi.com/wsr/1.1/)  (need to add exact endpoint for lyrics search)
-// API Key: 1c9baf23b0d366711ac0096634f7b3e6
-// Documentation: [https://developer.musixmatch.com/documentation](https://developer.musixmatch.com/documentation)
-
-var musixMatchAPI_KEY = "1c9baf23b0d366711ac0096634f7b3e6";
-
-$.ajax({
-    type: "GET",
-    url: "https://musixmatchcom-musixmatch.p.rapidapi.com/wsr/1.1/..." // Elipses to signify incomplete link. Not sure which exact endpoint to use.
+// jQuery for carousel
+$(document).ready(function() {
+    $('.carousel').carousel();
 });
 
-// YouTube
-// Endpoint: [https://www.youtube.com/iframe_api](https://www.youtube.com/iframe_api)
-// API Key: AIzaSyARIAx0ZVpwc63Bv3dptmWymN5DmCMOYG0
-// Documentation: [https://developers.google.com/youtube/v3/docs/](https://developers.google.com/youtube/v3/docs/)
 
-var youTubeAPI_KEY = "AIzaSyARIAx0ZVpwc63Bv3dptmWymN5DmCMOYG0";
 
-$.ajax({
-    type: "GET",
-    url: "https://www.youtube.com/iframe_api]"
-});
+// // MusixMatch
+// // Endpoint: [https://musixmatchcom-musixmatch.p.rapidapi.com/wsr/1.1/](https://musixmatchcom-musixmatch.p.rapidapi.com/wsr/1.1/)  (need to add exact endpoint for lyrics search)
+// // API Key: 1c9baf23b0d366711ac0096634f7b3e6
+// // Documentation: [https://developer.musixmatch.com/documentation](https://developer.musixmatch.com/documentation)
+// // Search for a string among lyrics: q_lyrics
 
-// NFLArrest
-// Endpoint: [http://nflarrest.com/api/v1/](http://nflarrest.com/api/v1/)
-// API Key: a81d696ff0msh9be012b0b7f7f37p1ddc64jsn4a73ad761f23 (from Rapid-API)
-// Documentation: [http://nflarrest.com/api/](http://nflarrest.com/api/)
 
-var nflArrestAPI_KEY = "a81d696ff0msh9be012b0b7f7f37p1ddc64jsn4a73ad761f23";
+// var userInput = "";
 
-$.ajax({
-    type: "GET",
-    url: "http://nflarrest.com/api/v1/..." // Elipses to signify incomplete link. Not sure which exact endpoint to use.
-});
+// var musixMatchAPI_KEY = "1c9baf23b0d366711ac0096634f7b3e6";
+
+// $.ajax({
+//     url: "https://musixmatchcom-musixmatch.p.rapidapi.com/wsr/1.1/?q=" + userInput + "?apikey=" + musixMatchAPI_KEY,
+//     method: "GET"
+// }).then(function(response) {
+//     console.log(response);
+// });
+
+// // YouTube
+// // Endpoint: [https://www.youtube.com/iframe_api](https://www.youtube.com/iframe_api)
+// // API Key: AIzaSyARIAx0ZVpwc63Bv3dptmWymN5DmCMOYG0
+// // Documentation: [https://developers.google.com/youtube/v3/docs/](https://developers.google.com/youtube/v3/docs/)
+
+// // var youTubeAPI_KEY = "AIzaSyARIAx0ZVpwc63Bv3dptmWymN5DmCMOYG0";
+
+// // $.ajax({
+// //     type: "GET",
+// //     url: "https://www.youtube.com/iframe_api"
+// // });
+
+// // NFLArrest
+// // Endpoint: [http://nflarrest.com/api/v1/](http://nflarrest.com/api/v1/)
+// // API Key: a81d696ff0msh9be012b0b7f7f37p1ddc64jsn4a73ad761f23 (from Rapid-API)
+// // Documentation: [http://nflarrest.com/api/](http://nflarrest.com/api/)
+
+// var nflArrestAPI_KEY = "a81d696ff0msh9be012b0b7f7f37p1ddc64jsn4a73ad761f23";
+
+// $.ajax({
+//     type: "GET",
+//     url: "http://nflarrest.com/api/v1/..." // Elipses to signify incomplete link. Not sure which exact endpoint to use.
+// });
